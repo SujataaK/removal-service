d19e5528f63f8b748252b1b87f293a297df15186
unknown <sujatakunwar201@gmail.com>
2025-04-18 09:03:48 +0545

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// toggle navbar on mobile view
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".testimonial-slide");
+    const prevBtn = document.querySelector(".left-arrow");
+    const nextBtn = document.querySelector(".right-arrow");
+
+    function showSlide(index) {
+      slides.forEach((slide) => slide.classList.remove("active"));
+      slides[index].classList.add("active");
+    }
+
+    prevBtn.addEventListener("click", () => {
+      slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+      showSlide(slideIndex);
+    });
+
+    nextBtn.addEventListener("click", () => {
+      slideIndex = (slideIndex + 1) % slides.length;
+      showSlide(slideIndex);
+    });
+
+    showSlide(slideIndex); // initial
+  });
