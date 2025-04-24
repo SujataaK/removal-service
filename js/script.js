19c1d04f9cf3c75a04882f378dea04d286f22bd4
unknown <sujatakunwar201@gmail.com>
2025-04-24 21:48:07 +0545

    commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function toggleMenu() {
   navLinks.classList.toggle('active');
 }
 
-
+// carousoul for testimonial slides
 document.addEventListener("DOMContentLoaded", function () {
     let slideIndex = 0;
     const slides = document.querySelectorAll(".testimonial-slide");
@@ -27,5 +27,5 @@ document.addEventListener("DOMContentLoaded", function () {
       showSlide(slideIndex);
     });
 
-    showSlide(slideIndex); // initial
+    showSlide(slideIndex); 
   });
